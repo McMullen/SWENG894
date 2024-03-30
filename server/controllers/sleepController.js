79900d1118404df2350283cd742009bde493a6f2
McMullen, Jason (HII-Mission Technologies) <jason.mcmullen@hii-tsd.com>
2024-03-30 09:18:04 -0400

    Cleaning up routes.

---
+++ server/controllers/sleepController.js
@@ -0,0 +1,18 @@
+const SleepModel = require('../models/SleepModel');
+const authService = require('../services/authService');
+
+exports.newSleep = async(req, res) => {
+    //
+};
+
+exports.updateSleep = async(req, res) => {
+    //
+};
+
+exports.getSleep = async(req, res) => {
+    //
+};
+
+exports.getAllSleeps = async(req, res) => {
+    //
+};
